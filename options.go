f2b286a9390e5604f26beeffcb8fab453dde445f
Adhiana <adhianamastur48@gmail.com>
2023-05-06 06:12:34 +0700

    feat: batcher default config, size, and wait

--- options.go
+++ options.go
@@ -1,7 +1,41 @@
 package batchease
 
+import (
+	"time"
+)
+
 // BatcherOption
 type BatcherOption[T any] func(b *Batcher[T]) error
+
+func WithDefaultConfig[T any]() BatcherOption[T] {
+	return func(b *Batcher[T]) error {
+		b.size = 5
+		b.wait = time.Second
+		b.loadBalancer = &RoundRobinLB[T]{}
+
+		return nil
+	}
+}
+
+func WithSize[T any](size int) BatcherOption[T] {
+	return func(b *Batcher[T]) error {
+		if size <= 0 {
+			return ErrInvalidBatchSize
+		}
+
+		b.size = size
+
+		return nil
+	}
+}
+
+func WithWait[T any](wait time.Duration) BatcherOption[T] {
+	return func(b *Batcher[T]) error {
+		b.wait = wait
+
+		return nil
+	}
+}
 
 // WorkerOption
 type WorkerOption[T any] func(w *Worker[T]) error
