d52d4c39818e0f1ad7e773f6c6faa99da6f4f324
Adhiana <adhianamastur48@gmail.com>
2023-05-06 06:11:26 +0700

    feat: create round robin loadbalancer

--- loadbalancer.go
+++ loadbalancer.go
@@ -3,3 +3,20 @@ package batchease
 type LoadBalancer[T any] interface {
 	Resolve(workers []*Worker[T]) *Worker[T]
 }
+
+type RoundRobinLB[T any] struct {
+	// current index
+	i int32
+}
+
+func (lb *RoundRobinLB[T]) Resolve(workers []*Worker[T]) *Worker[T] {
+	var n int32 = int32(len(workers))
+
+	if n <= 1 {
+		return workers[0]
+	}
+
+	lb.i = (lb.i + 1) % n
+
+	return workers[lb.i]
+}
